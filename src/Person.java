ba1ada13d0680e141c28db4d8b6037f5c22eb712
Lucas-AVS <lucas.alencar31@gmail.com>
2024-06-17 17:03:12 -0300

    feat: new Person class

---
+++ src/Person.java
@@ -0,0 +1,66 @@
+public class Person {
+    protected int ID;
+    protected String name;
+    protected int age;
+    protected int phone;
+    protected String email;
+    protected String bloodType;
+
+    public Person(int ID, String name, int age, int phone, String email, String bloodType) {
+        this.ID = ID;
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.email = email;
+        this.bloodType = bloodType;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int iD) {
+        ID = iD;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+}
